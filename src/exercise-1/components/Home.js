1f4c385c7c838d3706f65da8cab06d4651826c40
xiaowen <846703636@qq.com>
2020-09-23 19:38:21 +0800

    Home page finish

---
+++ src/exercise-1/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Home extends React.Component{
+    render(){
+        return <div>
+            <p>This is a beautiful Home page.</p>
+            <p>And the url is '/'.</p>
+        </div>
+    }
+}
+
+
+export default Home;
